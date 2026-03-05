3368fc697de6bd3f03e6219b7677f1b5307a6efd
Aminata-traore <166010838+Aminata-traore@users.noreply.github.com>
2026-03-05 16:14:20 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import RootLayoutClient from "@/components/layout/RootLayoutClient"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -9,15 +10,11 @@ export const metadata: Metadata = {
   description: "Système de Digitalisation des Fiches Statistiques",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fr">
+    <html lang="fr" suppressHydrationWarning>
       <body className={inter.className}>
-        {children}
+        <RootLayoutClient>{children}</RootLayoutClient>
       </body>
     </html>
   )
