ffdb1bd8c85b31e696191cbfe7bfbdbd665d83cb
Aminata-traore <166010838+Aminata-traore@users.noreply.github.com>
2026-03-05 19:41:16 +0000

    Update page.tsx

--- app/admin/fiche1/page.tsx
+++ app/admin/fiche1/page.tsx
@@ -2,6 +2,6 @@ export const dynamic = "force-dynamic";
 
 import { AdminFiche1Client } from "./AdminFiche1Client";
 
-export default function AdminFiche1Page() {
+export default function AdminPage() {
   return <AdminFiche1Client />;
 }
