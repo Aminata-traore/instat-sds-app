56f27125eda86124491217ca9b6dde5586fcc9d9
Aminata-traore <166010838+Aminata-traore@users.noreply.github.com>
2026-03-10 00:37:58 +0000

    Create AdminStatsChart.tsx

---
+++ components/charts/AdminStatsChart.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+export function AdminStatsChart({
+  data,
+}: {
+  data: { name: string; total: number }[];
+}) {
+  return (
+    <div className="h-80 w-full">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey="total" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
